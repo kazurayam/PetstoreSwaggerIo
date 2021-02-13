9d6c5e525c7065a6bc911e27a0c4a0f509108420
kazurayam <kazuaki.urayama@gmail.com>
2021-02-13 17:55:27 +0900

    updates

--- Scripts/multiple_searches_for_pets_by_status/Script1613178740584.groovy
+++ Scripts/multiple_searches_for_pets_by_status/Script1613178740584.groovy
@@ -4,20 +4,26 @@ import com.kms.katalon.core.testobject.RequestObject
 import com.kms.katalon.core.testobject.ResponseObject
 import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import groovy.json.JsonOutput
 
-def statusList = ["available", "pending", "sold"]
+println "multiple_searches_for_pets_by_status"
+
+def statusList = ["pending", "sold"]
 
 for (status in statusList) {
 	println "-------------------- ${status} pets --------------------"
 	String doc = retrievePetsByStatus(status)
-	println doc
+	println JsonOutput.prettyPrint(doc)
 }
 
 def retrievePetsByStatus(String status) {
 	TestObject tObj = findTestObject("Object Repository/my/petstore_findByStatus_parameterized", ["status": status])
-	RequestObject req = (RequestObject)tObj
-	ResponseObject res = WS.sendRequest(req)
-	return res.getResponseBodyContent()
+	if (tObj instanceof RequestObject) {
+		RequestObject req = (RequestObject)tObj
+		ResponseObject res = WS.sendRequest(req)
+		return res.getResponseBodyContent()
+	} else
+		throw new IllegalArgumentException("not a RequestObject")
 }
 
 
